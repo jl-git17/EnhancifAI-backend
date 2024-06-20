a0692e4c0b55bb8fcc1f49b1391c790bd240425b
imwatsi <31913074+imwatsi@users.noreply.github.com>
2024-06-20 13:57:04 +0200

    Update google_sheets_routes.py

--- enhancifai_backend/server/routes/google_sheets_routes.py
+++ enhancifai_backend/server/routes/google_sheets_routes.py
@@ -14,7 +14,7 @@ router = APIRouter()
 # Load client secrets from environment variable
 client_secrets_json = os.getenv("GOOGLE_TOKEN_INFO_AUTH")
 print(client_secrets_json)
-if not client_secrets_json:
+if client_secrets_json is None:
     raise ValueError("GOOGLE_TOKEN_INFO_AUTH environment variable not set")
 
 client_secrets = json.loads(client_secrets_json)
