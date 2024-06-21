6ca06aa4dde6e0d17d8d0c61d623b2ea102212d2
imwatsi <31913074+imwatsi@users.noreply.github.com>
2024-06-21 19:08:48 +0200

    Update google_sheets_routes.py

--- enhancifai_backend/server/routes/google_sheets_routes.py
+++ enhancifai_backend/server/routes/google_sheets_routes.py
@@ -8,6 +8,7 @@ from google_auth_oauthlib.flow import Flow
 from enhancifai_backend.database.handlers.runs import RunsDbCore
 from enhancifai_backend.database.handlers.sheets import SheetsDbCore
 from enhancifai_backend.database.handlers.users import UsersDbCore
+from enhancifai_backend.server.models.execution import ExportSheetsRequest
 from enhancifai_backend.server.utils import get_current_user_id, verify_secret_key
 from enhancifai_backend.engine.export_google_sheets import export_to_google_sheets
 
@@ -76,12 +77,12 @@ def creds_to_dict(creds):
     }
 
 @router.post("/sheets/export", tags=["Sheets"])
-async def export_to_sheets(run_id: int, user_id: Optional[int] = Depends(get_current_user_id), _: str = Depends(verify_secret_key)):
+async def export_to_sheets(req_sheets: ExportSheetsRequest, user_id: Optional[int] = Depends(get_current_user_id), _: str = Depends(verify_secret_key)):
     if not user_id:
         raise HTTPException(status_code=401, detail="User not authenticated")
 
     # Assuming you have a method to get the file path from the run_id
-    file_path = RunsDbCore.get_run_details(run_id)
+    file_path = RunsDbCore.get_run_details(req_sheets.run_id)
     if not file_path:
         raise HTTPException(status_code=404, detail="Run not found or file path not available")
 
