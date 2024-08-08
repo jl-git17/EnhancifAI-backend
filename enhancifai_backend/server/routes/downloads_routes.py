c30f015af2999c756b3e11c19fb00feb717dedee
imwatsi <31913074+imwatsi@users.noreply.github.com>
2024-08-08 15:59:55 +0200

    refactor and cleanup downloads_routes.py

--- enhancifai_backend/server/routes/downloads_routes.py
+++ enhancifai_backend/server/routes/downloads_routes.py
@@ -1,12 +1,15 @@
 
 import mimetypes
 import os
-from typing import Optional
 from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import FileResponse, JSONResponse
 
 from enhancifai_backend.database.handlers.users import UsersDbCore
-from enhancifai_backend.server.utils import STATIC_FILES_DIRECTORY, get_current_user_id, verify_secret_key
+from enhancifai_backend.server.utils import (
+    STATIC_FILES_DIRECTORY,
+    get_current_user_id,
+    verify_secret_key
+)
 
 
 router = APIRouter()
@@ -17,7 +20,11 @@ async def download_prompts_template(_: str = Depends(verify_secret_key)):
     try:
         file_path = os.path.join(STATIC_FILES_DIRECTORY, "prompts_template.xlsx")
         if os.path.exists(file_path):
-            return FileResponse(file_path, media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', filename="prompts_template.xlsx")
+            return FileResponse(
+                file_path,
+                media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                filename="prompts_template.xlsx"
+            )
         else:
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -27,21 +34,27 @@ async def download_prompts_template(_: str = Depends(verify_secret_key)):
         return JSONResponse(status_code=e.status_code, content={"detail": e.detail})
     except Exception as e:
         # Catch-all for unexpected errors
-        return JSONResponse(status_code=500, content={"detail": "An unexpected error occurred", "error": str(e)})
-    
+        return JSONResponse(
+            status_code=500,
+            content={"detail": "An unexpected error occurred", "error": str(e)}
+        )
+
 @router.get("/downloads/{filename}", tags=["Downloads"])
-async def download_file(filename: str, _: str = Depends(verify_secret_key), 
+async def download_file(filename: str, _: str = Depends(verify_secret_key),
                         user_id: int = Depends(get_current_user_id)):
     try:
         ai_consent = UsersDbCore.check_ai_consent(user_id)
         if ai_consent is False:
-            raise HTTPException(status_code=status.HTTP_451_UNAVAILABLE_FOR_LEGAL_REASONS, detail="User has not consented for AI usage.")
+            raise HTTPException(
+                status_code=status.HTTP_451_UNAVAILABLE_FOR_LEGAL_REASONS,
+                detail="User has not consented for AI usage."
+            )
         file_path = os.path.join('/tmp', filename)
         if os.path.exists(file_path):
             # Guess the MIME type of the file based on its extension
             mime_type, _ = mimetypes.guess_type(file_path)
             if mime_type is None:
-                mime_type = "application/octet-stream"  # Use a binary stream as a fallback MIME type
+                mime_type = "application/octet-stream"  # Use a binary stream as a fallback
 
             # Extract the file name for use in the Content-Disposition header
             file_name = os.path.basename(file_path)
@@ -53,4 +66,7 @@ async def download_file(filename: str, _: str = Depends(verify_secret_key),
         return JSONResponse(status_code=e.status_code, content={"detail": e.detail})
     except Exception as e:
         # Catch-all for unexpected errors
-        return JSONResponse(status_code=500, content={"detail": "An unexpected error occurred", "error": str(e)})
+        return JSONResponse(
+            status_code=500,
+            content={"detail": "An unexpected error occurred", "error": str(e)}
+        )
