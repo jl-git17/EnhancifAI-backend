8a2bc26f5c620d356c3c7260ac8f8771741879d8
imwatsi <31913074+imwatsi@users.noreply.github.com>
2024-10-29 21:52:06 +0200

    Revert "Update users.py"
    This reverts commit 79d8472c2057785bfaab688eef2fec8de7de4a09.

--- enhancifai_backend/database/handlers/users.py
+++ enhancifai_backend/database/handlers/users.py
@@ -73,7 +73,7 @@ class UsersDbCore:
         """
         sql = schemafy("SELECT ai_consent FROM enhancifai.users WHERE user_id = %s;")
         result = read_db.do('select_one', sql=sql, data=(user_id,))
-        return True if result else False
+        return result['ai_consent'].isoformat() if result else False
 
     @classmethod
     def update_ai_consent(cls, user_id):
