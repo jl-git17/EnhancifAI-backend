91460c27126dfceed4bcf279e7cb459be142ffa0
imwatsi <31913074+imwatsi@users.noreply.github.com>
2025-02-04 18:35:04 +0200

    refactor: add logging for completed run status in check_run_progress

--- enhancifai_backend/server/routes/execution_routes.py
+++ enhancifai_backend/server/routes/execution_routes.py
@@ -199,6 +199,7 @@ async def check_run_progress(req_run: RunProgressRequest, _: str = Depends(verif
                     _status["progress"] = "1"
                     _status["remark"] = "1% completed."
                 elif _status.get("status") == "completed":
+                    logging.info(f"Status: {_status}")
                     input_tokens = _status.get("results", {}).get("input_tokens_sum", 0)
                     output_tokens = _status.get("results", {}).get("output_tokens_sum", 0)
                     total_tokens_sum = input_tokens + output_tokens
