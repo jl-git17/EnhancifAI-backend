10f6b56bba7ebb0fd6f25f7b324718a4a1e17ca8
imwatsi <31913074+imwatsi@users.noreply.github.com>
2025-02-05 16:42:49 +0200

    refactor: update reset_billing.sql to truncate stripe_invoices with restart identity and cascade

--- enhancifai_backend/database/sql/reset_billing.sql
+++ enhancifai_backend/database/sql/reset_billing.sql
@@ -1,12 +1,11 @@
--- Delete all records from stripe_invoices
--- Truncate all records from tables with cascade to remove dependent rows
-TRUNCATE TABLE enhancifai.stripe_invoices--,
+TRUNCATE TABLE enhancifai.stripe_invoices
+RESTART IDENTITY CASCADE;
+--,
                --enhancifai.runs,
                --enhancifai.prompt_improver_run_logs,
                --enhancifai.users_token_usage,
                --enhancifai.users_token_usage_pi,
                --enhancifai.run_logs
-RESTART IDENTITY CASCADE;
 
 
 -- Reset the invoice_number_seq to start at 1
