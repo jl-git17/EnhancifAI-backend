e87f30d497b157ed0df0ce163223cf036486542a
imwatsi <31913074+imwatsi@users.noreply.github.com>
2025-02-13 14:32:19 +0200

    refactor: remove unused Google Sheets credential refresh function and imports

--- enhancifai_backend/server/jobs.py
+++ enhancifai_backend/server/jobs.py
@@ -1,7 +1,6 @@
 import os
 import time
-from enhancifai_backend.engine.import_google_sheets import GoogleSheetsHandler
-from enhancifai_backend.database.handlers.users import UsersDbCore
+
 from enhancifai_backend.server.utils import FILE_AGE_LIMIT
 
 def human_readable_duration(seconds):
@@ -13,19 +12,6 @@ def human_readable_duration(seconds):
         return f"{int(minutes)} minutes and {seconds:.2f} seconds"
     else:
         return f"{seconds:.2f} seconds"
-
-def refresh_google_sheets_creds():
-    start_time = time.time()  # Record the start time
-    creds_list = UsersDbCore.get_all_users_with_creds()
-    for cred in creds_list:
-        try:
-            GoogleSheetsHandler(cred['user_id'])
-        except Exception as e:
-            print(f"Error refreshing Google Sheets creds: (user_id > {cred['user_id']})\n{e}")
-    end_time = time.time()  # Record the end time
-    duration = end_time - start_time  # Calculate the duration
-    human_readable_time = human_readable_duration(duration)
-    print(f"refresh_google_sheets_creds started at {time.ctime(start_time)} and took {human_readable_time}")
 
 def delete_old_files():
     """
