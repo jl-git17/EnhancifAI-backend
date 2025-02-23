fcf5e5f22b74e07151288ce1348a272498f10c4f
imwatsi <31913074+imwatsi@users.noreply.github.com>
2025-02-23 22:06:35 +0200

    fix: format return statement in OpenAIConnector for improved readability

--- enhancifai_backend/ai/openai_api.py
+++ enhancifai_backend/ai/openai_api.py
@@ -416,7 +416,12 @@ class OpenAIConnector:
 
                 new_prompt = data.replace("```", "").strip()
                 UsersDbCore.add_user_token_usage_pi(user_id, self.engine, tokens_used)
-                return {"content": new_prompt, "input_tokens": input_tokens, "output_tokens": output_tokens, 'engine_used': self.engine}
+                return {
+                    "content": new_prompt,
+                    "input_tokens": input_tokens,
+                    "output_tokens": output_tokens,
+                    "engine_used": self.engine
+                }
 
             except json.JSONDecodeError:
                 # Handle the case where the response is not a valid JSON string
