76ad3d3700d11bf749747581fbe7dc0184021ad2
imwatsi <31913074+imwatsi@users.noreply.github.com>
2025-02-23 22:06:50 +0200

    refactor: remove unused email sending methods in SendGrid class for cleaner code

--- enhancifai_backend/integrations/sendgrid_api.py
+++ enhancifai_backend/integrations/sendgrid_api.py
@@ -9,26 +9,6 @@ PASSWORD_RESET_TEMPLATE = "d-75c53f9a77304ae480856b429ef8ff7c"
 class SendGrid:
 
     @classmethod
-    def send_social_account_ready_email(cls, to_email, login_url):
-        # create Mail object and populate
-        message = Mail(
-            from_email="info@enhancifai.com",
-            to_emails=[to_email])
-        # pass custom values for our HTML placeholders
-        message.dynamic_template_data = {
-            'login_url': login_url
-        }
-        message.template_id = SOCIAL_ACCOUNT_READY_TEMPLATE
-        # create our sendgrid client object, pass it our key, then send and return our response objects
-        try:
-            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-            response = sg.send(message)
-            _code, _body, _headers = response.status_code, response.body, response.headers
-            print("Dynamic Messages Sent!")
-        except Exception as e:
-            print(f"Error: {e}")
-
-    @classmethod
     def send_registration_email(cls, to_email, token, name):
         # create Mail object and populate
         message = Mail(
@@ -91,3 +71,15 @@ class SendGrid:
             print("Dynamic Messages Sent!")
         except Exception as e:
             print(f"Error: {e}")
+
+    @classmethod
+    def send_invoice_payment_success_email(cls, to_email):
+        pass
+
+    @classmethod
+    def send_invoice_payment_failure_email(cls, to_email):
+        pass
+
+    @classmethod
+    def send_subscription_start_email(cls, to_email):
+        pass
