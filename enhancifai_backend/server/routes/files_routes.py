8df3d025c1253b4efb5dbb0d8729543f088a0521
imwatsi <31913074+imwatsi@users.noreply.github.com>
2025-06-28 18:50:26 +0200

    feat: enhance cache handling by adding support for free cache directory

--- enhancifai_backend/server/routes/files_routes.py
+++ enhancifai_backend/server/routes/files_routes.py
@@ -7,31 +7,33 @@ from fastapi.responses import FileResponse
 
 from enhancifai_backend.database.handlers.users import UsersDbCore
 from enhancifai_backend.server.models.execution import CacheRequest
-from enhancifai_backend.server.utils import get_current_user_id, verify_secret_key
+from enhancifai_backend.server.utils import CACHE_DIRECTORY, CACHE_DIRECTORY_FREE, get_current_user_id, verify_secret_key
 
-
-CACHE_DIRECTORY = '/tmp/enhancifai_cache'
 
 router = APIRouter()
 
-def ensure_cache_directory():
-    if not os.path.exists(CACHE_DIRECTORY):
-        os.makedirs(CACHE_DIRECTORY)
+def ensure_cache_directory(free: bool = False):
+    if free:
+        if not os.path.exists(CACHE_DIRECTORY_FREE):
+            os.makedirs(CACHE_DIRECTORY_FREE)
+    else:
+        if not os.path.exists(CACHE_DIRECTORY):
+            os.makedirs(CACHE_DIRECTORY)
 
-def get_cache_file_path(user_id, filename):
-    ensure_cache_directory()
-    user_dir = os.path.join(CACHE_DIRECTORY, str(user_id))
+def get_cache_file_path(user_id, filename, free: bool = False):
+    ensure_cache_directory(free)
+    user_dir = os.path.join(CACHE_DIRECTORY_FREE if free else CACHE_DIRECTORY, str(user_id))
     os.makedirs(user_dir, exist_ok=True)
     return os.path.join(user_dir, filename)
 
-def save_to_cache(file_path, user_id, filename):
-    cache_path = get_cache_file_path(user_id, filename)
+def save_to_cache(file_path, user_id, filename, free: bool = False):
+    cache_path = get_cache_file_path(user_id, filename, free)
     os.makedirs(os.path.dirname(cache_path), exist_ok=True)
     shutil.copy(file_path, cache_path)
     return cache_path
 
-def get_from_cache(user_id, filename):
-    cache_path = get_cache_file_path(user_id, filename)
+def get_from_cache(user_id, filename, free: bool = False):
+    cache_path = get_cache_file_path(user_id, filename, free)
     if os.path.exists(cache_path):
         return cache_path
     return None
